77956bcdef0af143e06d53da5ba0f80b61b384c9
andrew-leith <andrew-leith@users.noreply.github.com>
2017-12-18 14:49:34 -0500

    added config script

---
+++ configure_cbc_conda.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo 'export PATH=/gpfs/data/cbc/cbc_conda_v1/bin:$PATH' >> ~/.bash_profile
+
+source ~/.bash_profile
---
+++ modify_path.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo 'export PATH=/gpfs/data/cbc/cbcollab/anaconda2/bin:$PATH' >> ~/.bash_profile
